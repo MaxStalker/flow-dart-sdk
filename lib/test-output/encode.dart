0c866530ea096797fffa07dcc1763e6e1b6ac2ce
Max Daunarovich <max.daunarovich@gmail.com>
2021-05-10 00:50:48 +0300

    Check more in encode

--- lib/test-output/encode.dart
+++ lib/test-output/encode.dart
@@ -1,13 +1,17 @@
-import 'dart:convert';
+import 'dart:typed_data';
 import 'package:convert/convert.dart';
 import 'package:rlp/rlp.dart';
+import 'package:pointycastle/pointycastle.dart';
 
 void main() {
   final code = '''transaction { execute { log("Hello, World!") } }''';
   final fullAddress = "0x"+"01".padLeft(16, '0');
-  print(fullAddress);
   final address = Address(fullAddress);
   print(address);
+
+  print("test");
+  final testAddress = hex.encode(hex.decode("01".padLeft(16,"0")));
+  print(Address(testAddress));
 
   final refBlock = Address(
       "0x"+"f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b"
@@ -33,9 +37,13 @@ void main() {
 
   print(hex.encode(Rlp.encode(code)));
 
-  final result = hex.encode(Rlp.encode(payload));
+  final rlpEncoded = Rlp.encode(payload);
+  final result = hex.encode(rlpEncoded);
   final ref = "f872b07472616e73616374696f6e207b2065786563757465207b206c6f67282248656c6c6f2c20576f726c64212229207d207dc0a0f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b2a880000000000000001040a880000000000000001c9880000000000000001";
   print(result);
   print(ref);
   print(result == ref);
+
+  final payloadSignature = new Digest('SHA-3/256').process(Uint8List.fromList(rlpEncoded));
+  print(payloadSignature);
 }
