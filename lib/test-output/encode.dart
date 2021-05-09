bbdfce2ad36fd94641bd25fa36238d353e97828f
Max Daunarovich <max.daunarovich@gmail.com>
2021-05-09 23:01:13 +0300

    Fix payload encoding

--- lib/test-output/encode.dart
+++ lib/test-output/encode.dart
@@ -3,12 +3,16 @@ import 'package:convert/convert.dart';
 import 'package:rlp/rlp.dart';
 
 void main() {
-  final code =
-      utf8.encode('''transaction { execute { log("Hello, World!") } }''');
-  final address = hex.decode("01".padLeft(16, '0'));
-  final refBlock = hex.decode(
-      "f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b"
+  final code = '''transaction { execute { log("Hello, World!") } }''';
+  final fullAddress = "0x"+"01".padLeft(16, '0');
+  print(fullAddress);
+  final address = Address(fullAddress);
+  print(address);
+
+  final refBlock = Address(
+      "0x"+"f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b"
           .padLeft(64, '0'));
+
   final computeLimit = 42;
   final keyIndex = 4;
   final seqNum = 10;
@@ -27,11 +31,11 @@ void main() {
 
   print(payload);
 
-  /*
-  print("f881f07472616e73616374696f6e207b2065786563757465207b206c6f67282248656c6c6f2c20576f726c64212229207d207dc0ef81f081e481c281f76c5881916e81c25881f24681851b81ea091d1481d4247a2f81c381e181868194461b181681e13b2ac88080808080808001040ac88080808080808001c9c88080808080808001" ==
-      "f881f07472616e73616374696f6e207b2065786563757465207b206c6f67282248656c6c6f2c20576f726c64212229207d207dc0ef81f081e481c281f76c5881916e81c25881f24681851b81ea091d1481d4247a2f81c381e181868194461b181681e13b2ac88080808080808001040ac88080808080808001c9c88080808080808001");
+  print(hex.encode(Rlp.encode(code)));
 
-   */
-
-  print(hex.encode(Rlp.encode(payload)));
+  final result = hex.encode(Rlp.encode(payload));
+  final ref = "f872b07472616e73616374696f6e207b2065786563757465207b206c6f67282248656c6c6f2c20576f726c64212229207d207dc0a0f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b2a880000000000000001040a880000000000000001c9880000000000000001";
+  print(result);
+  print(ref);
+  print(result == ref);
 }
