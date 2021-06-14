ef56cf7c549eab294efbb95423aab8e7d5eb2f79
Max Daunarovich <max.daunarovich@gmail.com>
2021-06-15 01:13:18 +0300

    Update tests

--- test/rlp_test.dart
+++ test/rlp_test.dart
@@ -39,6 +39,14 @@ Transaction baseTransaction() {
 
   transaction.authorizers.insertAll(0, [payer]);
 
+  final signature = Transaction_Signature()
+    ..signature = hex.decode(
+        "f7225388c1d69d57e6251c9fda50cbbf9e05131e5adb81e5aa0422402f048162")
+    ..address = payer
+    ..keyId = 4;
+
+  transaction.payloadSignatures.add(signature);
+
   return transaction;
 }
 
@@ -46,17 +54,24 @@ void main() {
   group('valid payload conversion to message', () {
     test('basic transaction', () {
       final tx = baseTransaction();
-      final actual = hex.encode(payloadToMessage(tx));
+      final payload = Rlp.encode(transactionPayload(tx));
+      final actual = hex.encode(payload);
       final reference =
           "f872b07472616e73616374696f6e207b2065786563757465207b206c6f67282248656c6c6f2c20576f726c64212229207d207dc0a0f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b2a880000000000000001040a880000000000000001c9880000000000000001";
       expect(actual, reference);
+
+      final referenenceEnvelope =
+          "f899f872b07472616e73616374696f6e207b2065786563757465207b206c6f67282248656c6c6f2c20576f726c64212229207d207dc0a0f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b2a880000000000000001040a880000000000000001c9880000000000000001e4e38004a0f7225388c1d69d57e6251c9fda50cbbf9e05131e5adb81e5aa0422402f048162";
+      final actualEnvelope = hex.encode(foldEnvelope(tx));
+      expect(actualEnvelope, referenenceEnvelope);
     });
 
     test('empty script', () {
       final tx = baseTransaction();
       tx.script = [];
 
-      final actual = hex.encode(payloadToMessage(tx));
+      final payload = Rlp.encode(transactionPayload(tx));
+      final actual = hex.encode(payload);
       final reference =
           "f84280c0a0f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b2a880000000000000001040a880000000000000001c9880000000000000001";
       expect(actual, reference);
@@ -66,7 +81,8 @@ void main() {
       final tx = baseTransaction();
       tx.referenceBlockId = [];
 
-      final actual = hex.encode(payloadToMessage(tx));
+      final payload = Rlp.encode(transactionPayload(tx));
+      final actual = hex.encode(payload);
       final reference =
           "f872b07472616e73616374696f6e207b2065786563757465207b206c6f67282248656c6c6f2c20576f726c64212229207d207dc0a000000000000000000000000000000000000000000000000000000000000000002a880000000000000001040a880000000000000001c9880000000000000001";
       expect(actual, reference);
@@ -76,7 +92,8 @@ void main() {
       final tx = baseTransaction();
       tx.gasLimit = Int64(0);
 
-      final actual = hex.encode(payloadToMessage(tx));
+      final payload = Rlp.encode(transactionPayload(tx));
+      final actual = hex.encode(payload);
       final reference =
           "f872b07472616e73616374696f6e207b2065786563757465207b206c6f67282248656c6c6f2c20576f726c64212229207d207dc0a0f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b80880000000000000001040a880000000000000001c9880000000000000001";
       expect(actual, reference);
@@ -86,7 +103,8 @@ void main() {
       final tx = baseTransaction();
       tx.proposalKey.keyId = 0;
 
-      final actual = hex.encode(payloadToMessage(tx));
+      final payload = Rlp.encode(transactionPayload(tx));
+      final actual = hex.encode(payload);
       final reference =
           "f872b07472616e73616374696f6e207b2065786563757465207b206c6f67282248656c6c6f2c20576f726c64212229207d207dc0a0f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b2a880000000000000001800a880000000000000001c9880000000000000001";
       expect(actual, reference);
@@ -96,7 +114,8 @@ void main() {
       final tx = baseTransaction();
       tx.proposalKey.sequenceNumber = Int64(0);
 
-      final actual = hex.encode(payloadToMessage(tx));
+      final payload = Rlp.encode(transactionPayload(tx));
+      final actual = hex.encode(payload);
       final reference =
           "f872b07472616e73616374696f6e207b2065786563757465207b206c6f67282248656c6c6f2c20576f726c64212229207d207dc0a0f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b2a8800000000000000010480880000000000000001c9880000000000000001";
       expect(actual, reference);
@@ -106,7 +125,8 @@ void main() {
       final tx = baseTransaction();
       tx.authorizers.insert(1, hex.decode("02"));
 
-      final actual = hex.encode(payloadToMessage(tx));
+      final payload = Rlp.encode(transactionPayload(tx));
+      final actual = hex.encode(payload);
       final reference =
           "f87bb07472616e73616374696f6e207b2065786563757465207b206c6f67282248656c6c6f2c20576f726c64212229207d207dc0a0f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b2a880000000000000001040a880000000000000001d2880000000000000001880000000000000002";
       expect(actual, reference);
@@ -120,7 +140,8 @@ void main() {
       final args = prepareArguments(arguments);
       tx.arguments.insertAll(0, args);
 
-      final actual = hex.encode(payloadToMessage(tx));
+      final payload = Rlp.encode(transactionPayload(tx));
+      final actual = hex.encode(payload);
       final reference =
           "f893b07472616e73616374696f6e207b2065786563757465207b206c6f67282248656c6c6f2c20576f726c64212229207d207de1a07b2274797065223a22537472696e67222c2276616c7565223a22666f6f227d0aa0f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b2a880000000000000001040a880000000000000001c9880000000000000001";
       expect(actual, reference);
@@ -135,12 +156,14 @@ void main() {
       final args = prepareArguments(arguments);
       tx.arguments.insertAll(0, args);
 
-      final actual = hex.encode(payloadToMessage(tx));
+      final payload = Rlp.encode(transactionPayload(tx));
+      final actual = hex.encode(payload);
       final reference =
           "f8b1b07472616e73616374696f6e207b2065786563757465207b206c6f67282248656c6c6f2c20576f726c64212229207d207df83ea07b2274797065223a22537472696e67222c2276616c7565223a22666f6f227d0a9c7b2274797065223a22496e74222c2276616c7565223a223432227d0aa0f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b2a880000000000000001040a880000000000000001c9880000000000000001";
       expect(actual, reference);
     });
 
+    /*
     test("signer", () {
       final List<int> message = [1, 2, 3, 4, 5, 12, 12, 44, 123, 412];
       final hashAlgo = "SHA3-256";
@@ -180,7 +203,6 @@ void main() {
 
       // var contractAddress = hex.encode(out.sublist(12));
     });
-
     test("RSA Signer", (){
       final pair = generateRSAkeyPair(exampleSecureRandom());
       final public = pair.publicKey;
@@ -226,5 +248,7 @@ void main() {
     test("cryptography signer", () async{
 
     });
+
+     */
   });
 }
