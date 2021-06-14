d9fa3ae8c517bb9027b8d834052babae40d666cd
Max Daunarovich <max.daunarovich@gmail.com>
2021-06-15 01:16:55 +0300

    Add signing implementation by bluesign

--- lib/fcl/crypto.dart
+++ lib/fcl/crypto.dart
@@ -1,6 +1,8 @@
-
+import 'dart:convert';
 import "dart:typed_data";
 import "dart:math";
+import 'package:flow_dart_sdk/fcl/constants.dart';
+import 'package:flow_dart_sdk/src/generated/flow/entities/transaction.pb.dart';
 import 'package:pointycastle/digests/sha3.dart';
 import "package:pointycastle/pointycastle.dart";
 import "package:pointycastle/export.dart";
@@ -8,7 +10,6 @@ import "package:pointycastle/api.dart";
 import "package:pointycastle/ecc/curves/prime256v1.dart";
 import "package:pointycastle/key_generators/api.dart";
 import "package:pointycastle/key_generators/ec_key_generator.dart";
-
 import 'package:pointycastle/src/impl/secure_random_base.dart';
 import 'package:pointycastle/src/registry/registry.dart';
 import 'package:pointycastle/src/ufixnum.dart';
@@ -19,23 +20,108 @@ import 'dart:typed_data';
 
 import "package:pointycastle/export.dart";
 
+/// Big thanks to Deniz Mert Edincik (https://github.com/bluesign)
+/// for help invaluable help with transaction signing!
+ByteData bigIntToByteData(BigInt bigInt, int padding) {
+  var data = ByteData(padding);
+  var _bigInt = bigInt;
+
+  for (var i = 1; i <= data.lengthInBytes; i++) {
+    data.setUint8(data.lengthInBytes - i, _bigInt.toUnsigned(8).toInt());
+    _bigInt = _bigInt >> 8;
+  }
+
+  return data;
+}
+
+Uint8List signatureToUint8List(ECSignature signature) {
+  final r = bigIntToByteData(signature.r, 32).buffer.asUint8List();
+  final s = bigIntToByteData(signature.s, 32).buffer.asUint8List();
+
+  var result = new Uint8List(64);
+
+  for (var i = 0; i < 32; i++) {
+    result[i] = r[i];
+  }
+  for (var i = 32; i < 64; i++) {
+    result[i] = s[i - 32];
+  }
+
+  return result;
+}
+
+Uint8List signData(List<int> input, String privateKey, String domainTag) {
+  List<int> data = [];
+  data.insertAll(0, input);
+
+  if (domainTag.length > 0) {
+    data.insertAll(0, utf8.encode(domainTag.padRight(32, "\x00")));
+  }
+
+  var privateParameters = new PrivateKeyParameter(new ECPrivateKey(
+      BigInt.parse(privateKey, radix: 16),
+      new ECDomainParameters('prime256v1')));
+
+  var signatureParams =
+      () => new ParametersWithRandom(privateParameters, new NullSecureRandom());
+
+  var signer = new ECDSASigner();
+  signer.reset();
+  signer.init(true, signatureParams());
+
+  var hasher = SHA3Digest(256);
+  final digest = hasher.process(Uint8List.fromList(data));
+
+  final ECSignature signature = signer.generateSignature(digest);
+  return signatureToUint8List(signature);
+}
+
+// Return signature from here
+void signPayload(
+    Transaction transaction,
+    List<int> payload,
+    List<int> signerAddress,
+    String privateKey,
+    int keyId,
+    List<dynamic> payloadSignatureHolder) {
+  var previousSignatures = transaction.payloadSignatures
+      .where((element) => element.address == signerAddress);
+  if (previousSignatures.length > 0) return;
+
+  final signature = signData(payload, privateKey, DOMAIN_TAG);
+
+  final payloadSignature = Transaction_Signature()
+    ..address = signerAddress
+    ..keyId = keyId
+    ..signature = signature;
+
+  transaction.payloadSignatures.add(payloadSignature);
+
+  final selected = transaction.payloadSignatures
+      .indexWhere((signature) => signature.address == signerAddress);
+
+  payloadSignatureHolder
+      .add([selected, keyId, String.fromCharCodes(signature)]);
+}
+
 /// An implementation of [SecureRandom] that return numbers in growing sequence.
 class NullSecureRandom extends SecureRandomBase {
   static final FactoryConfig factoryConfig =
-  StaticFactoryConfig(SecureRandom, 'Null', () => NullSecureRandom());
+      StaticFactoryConfig(SecureRandom, 'Null', () => NullSecureRandom());
 
   var _nextValue = 0;
+
   @override
   String get algorithmName => 'Null';
+
   @override
   void seed(CipherParameters params) {}
+
   @override
   int nextUint8() => clip8(_nextValue++);
 }
 
-void generate(){
-
-}
+void generate() {}
 
 AsymmetricKeyPair<PublicKey, PrivateKey> prime256v1KeyPair() {
   var keyParams = ECKeyGeneratorParameters(ECCurve_prime256v1());
@@ -55,19 +141,18 @@ Uint8List _seed() {
   return Uint8List.fromList(seed);
 }
 
-
 Uint8List rsaSign(RSAPrivateKey privateKey, Uint8List dataToSign) {
-
   final signer = RSASigner(SHA256Digest(), '0609608648016503040201');
 
-  signer.init(true, PrivateKeyParameter<RSAPrivateKey>(privateKey)); // true=sign
+  signer.init(
+      true, PrivateKeyParameter<RSAPrivateKey>(privateKey)); // true=sign
 
   final sig = signer.generateSignature(dataToSign);
 
   return sig.bytes;
 }
 
-Signature ecSign(ECPrivateKey privateKey, Uint8List dataToSign){
+Signature ecSign(ECPrivateKey privateKey, Uint8List dataToSign) {
   // final signer = ECDSASigner(SHA3Digest(256));
   final signer = new Signer("SHA3-256/DET-ECDSA");
 
@@ -121,4 +206,3 @@ Uint8List createUint8ListFromString(String s) {
   }
   return ret;
 }
-
